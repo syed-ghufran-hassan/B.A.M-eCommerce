a180047de9e8c294ad650dd37f513ecd811caa0a
Mauricio Lizama <mlizamaoliger@gmail.com>
2021-06-24 17:36:52 +0200

    Adding basic card model that we can chenge later and categories with some styles

---
+++ src/features/Pages/Cards.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Card.css';
+
+function Cards() {
+  return (
+    <div className="Card">
+      <img src="#" alt="any product">
+      </img>
+      <h1>Tailored Jeans</h1>
+      <p class="Price">$19.99</p>
+      <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
+      <p><button>Add to Cart</button></p>
+    </div>
+  );
+}
+
+export default Cards;
--- src/features/Pages/CategoryPage.js
+++ src/features/Pages/CategoryPage.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import "../styles/CategoryPage.css"
+import '../styles/CategoryPage.css';
+import Cards from './Cards'
 
 function CategoryPage() {
   return (
-    <div>
-      <h1>kjnc</h1>
+    <div className="Container">
+      <div className="Products-Title">
+        <h1>Our Products Categories</h1>
+      </div>
+      <div className="Categories-Products">
+        <div className="Categories">
+          <h3 className="Category-Title">Category</h3>
+          <button>Women Clothing</button>
+          <button>Men Clothing</button>
+        </div>
+        <div className="Cards-Container">
+          <Cards />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default CategoryPage
+export default CategoryPage;
