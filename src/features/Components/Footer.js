39834ce1a655f2b2182d6146e44068b2250df936
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-06-25 12:32:22 +0200

    Fixed the issue with the links and errors in console log

--- src/features/Components/Footer.js
+++ src/features/Components/Footer.js
@@ -2,58 +2,96 @@ import React from "react";
 import "../styles/Footer.css";
 import logo from "../img/logoW.png";
 import { Link } from "react-router-dom";
-import {
-  FaFacebook,
-  FaInstagramSquare,
-  FaYoutube
-} from "react-icons/fa";
+import { FaFacebook, FaInstagramSquare, FaYoutube } from "react-icons/fa";
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="Footer-background">
       <div className="Footer">
-      <div className="Links-wrapper">
-        <div className="Logo-part">
-          <img src={logo} alt=""></img>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Perferendis, dignissimos cum!
-          </p>
-          <div className="Footer-icons">
-          <Link className="icon facebook-icon">
-          <FaFacebook />
-        </Link>
-        <Link className="icon instagram-icon">
-          <FaInstagramSquare />
-        </Link>
-        <Link className="icon youtube-icon">
-          <FaYoutube />
-        </Link>
+        <div className="Links-wrapper">
+          <div className="Logo-part">
+            <img src={logo} alt=""></img>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Perferendis, dignissimos cum!
+            </p>
+            <div className="Footer-icons">
+              <Link
+                to={{ pathname: "https://www.facebook.com/" }}
+                target="_blank"
+                className="icon facebook-icon"
+              >
+                <FaFacebook />
+              </Link>
+              <Link
+                to={{ pathname: "https://www.instagram.com/" }}
+                target="_blank"
+                className="icon instagram-icon"
+              >
+                <FaInstagramSquare />
+              </Link>
+              <Link
+                to={{ pathname: "https://www.youtube.com/" }}
+                target="_blank"
+                className="icon youtube-icon"
+              >
+                <FaYoutube />
+              </Link>
+            </div>
+          </div>
+          <div className="Quick-links">
+            <h4>Quick Links</h4>
+            <Link
+              to={{ pathname: "https://theuselessweb.com/" }}
+              target="_blank"
+            >
+              Image Licensin
+            </Link>
+            <Link
+              to={{ pathname: "https://theuselessweb.com/" }}
+              target="_blank"
+            >
+              Style Guide
+            </Link>
+            <Link
+              to={{ pathname: "https://theuselessweb.com/" }}
+              target="_blank"
+            >
+              Privacy Policy
+            </Link>
+          </div>
+          <div className="Partners-links">
+            <h4>Partners Links</h4>
+            <Link
+              to={{ pathname: "https://en.zalando.de/?_rfl=de" }}
+              target="_blank"
+            >
+              Zalando
+            </Link>
+            <Link
+              to={{ pathname: "http://www.amazon.de" }}
+              target="_blank"
+            >
+              Amazon
+            </Link>
+            <Link
+              to={{ pathname: "https://www.ebay.de/" }}
+              target="_blank"
+            >
+              E-bay
+            </Link>
+          </div>
+          <div className="Contact-info">
+            <h4>Get in touch</h4>
+            <span>(09) 533 7732 20</span>
+            <span>bam-cat@startup.com</span>
+            <span>16/C Fugger Strasse, Potsdam, Germany</span>
           </div>
         </div>
-        <div className="Quick-links">
-          <h4>Quick Links</h4>
-          <Link>Image Licensin</Link>
-          <Link>Style Guide</Link>
-          <Link>Privacy Policy</Link>
+        <div className="Credentials">
+          <span> BAM E-Commerce | Copyright &copy; {year} </span>
         </div>
-        <div className="Partners-links">
-          <h4>Partners Links</h4>
-          <Link>Zalando</Link>
-          <Link>Amazon</Link>
-          <Link>E-bay</Link>
-        </div>
-        <div className="Contact-info">
-        <h4>Get in touch</h4>
-          <span>(09) 533 7732 20</span>
-          <span>bam-cat@startup.com</span>
-          <span>16/C Fugger Strasse, Potsdam, Germany</span>
-        </div>
-      </div>
-      <div className="Credentials">
-        <span> BAM E-Commerce | Copyright &copy; {year} </span>
-      </div>
       </div>
     </footer>
   );
--- src/features/Components/Navbar.js
+++ src/features/Components/Navbar.js
@@ -6,7 +6,6 @@ import {
   FaFacebook,
   FaInstagramSquare,
   FaYoutube,
-  FaSearch,
   FaShoppingBag,
 } from "react-icons/fa";
 
@@ -14,20 +13,20 @@ function Navbar() {
   return (
     <div className="NavbarWrapper">
       <div className="Navbar">
-      <NavLink
+        <NavLink
           className="headerLink"
           activeClassName="is-active"
           activeStyle={{ color: "hotpink" }}
-          exact="true"
+          exact
           to="/"
         >
-         <img className="logo" src={logo} alt=""></img>
+          <img className="logo" src={logo} alt=""></img>
         </NavLink>
         <NavLink
           className="headerLink"
           activeClassName="is-active"
           activeStyle={{ color: "hotpink" }}
-          exact="true"
+          exact
           to="/"
         >
           HOME
@@ -36,6 +35,7 @@ function Navbar() {
           className="headerLink"
           activeClassName="is-active"
           activeStyle={{ color: "hotpink" }}
+          exact
           to="/category"
         >
           CATEGORY
@@ -44,25 +44,39 @@ function Navbar() {
           className="headerLink"
           activeClassName="is-active"
           activeStyle={{ color: "hotpink" }}
+          exact
           to="/product"
         >
           PRODUCT
         </NavLink>
       </div>
       <div className="NavbarSocial">
-        <Link className="icon facebook-icon">
+        <Link
+          to={{ pathname: "https://www.facebook.com/" }}
+          target="_blank"
+          className="icon facebook-icon"
+        >
           <FaFacebook />
         </Link>
-        <Link className="icon instagram-icon">
+        <Link
+          to={{ pathname: "https://www.instagram.com/" }}
+          target="_blank"
+          className="icon instagram-icon"
+        >
           <FaInstagramSquare />
         </Link>
-        <Link className="icon youtube-icon">
+        <Link
+          to={{ pathname: "https://www.youtube.com/" }}
+          target="_blank"
+          className="icon youtube-icon"
+        >
           <FaYoutube />
         </Link>
-        <Link className="icon search-icon">
-          <FaSearch />
-        </Link>
-        <Link className="icon shopping-icon">
+        <Link
+          to={{ pathname: "https://www.youtube.com/" }}
+          target="_blank"
+          className="icon shopping-icon"
+        >
           <FaShoppingBag />
         </Link>
       </div>
