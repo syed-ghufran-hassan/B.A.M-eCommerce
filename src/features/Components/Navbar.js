fbc67f310a5848ccd232ca41fcbe7fe490105ee0
Nastasiia-Smr <volhvina@outlook.com>
2021-06-25 20:35:55 +0200

    update logo

--- src/features/Components/Navbar.js
+++ src/features/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../img/logo.png";
+import logo from "../img/logo.svg";
 import {
   FaFacebook,
   FaInstagramSquare,
--- src/features/Components/ProductModal.js
+++ src/features/Components/ProductModal.js
@@ -1,4 +1,4 @@
-import logo from "../img/logo.png";
+import logo from "../img/logo.svg";
 
 function ProductModal(props) {
   const handleClose = () => {
