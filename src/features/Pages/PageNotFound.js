bf0c84ec10cfa26147d7c26ed37bb9a1a0399f21
Mauricio Lizama <mlizamaoliger@gmail.com>
2021-06-25 17:49:58 +0200

    Adding some random content to the PageNotFound

--- src/features/Pages/PageNotFound.js
+++ src/features/Pages/PageNotFound.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import "../styles/PageNotFound.css"
+import { Container, Row, Col } from 'react-bootstrap';
 
 function PageNotFound() {
   return (
-    <div>
-      
-    </div>
+    <Container fluid='md' className='mt-5'>
+      <Row>
+        <Col>
+          <div className='card'>
+            <div className='card-header'>404</div>
+            <div className='card-body'>
+              <h5 className='card-title'>Custom Error Page</h5>
+              <p className='card-text'>
+                Edit Custom 404 error page <code>src/pages/NotFound.js</code>
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
