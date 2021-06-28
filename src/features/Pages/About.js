d13fcacaac23cf049087bf201fbf21db3fc1a446
Mauricio Lizama <mlizamaoliger@gmail.com>
2021-06-28 21:43:25 +0200

    Minor change in About text

--- src/features/Pages/About.js
+++ src/features/Pages/About.js
@@ -11,7 +11,7 @@ export default function About() {
       </div>
       <div className="About-Container">
         <div className="Description-Container">
-          <p className="Description-About"><span style={{ color: "#623BC1", fontSize: "20px", fontWeight: "bolder"}}>BAM CAT</span> is a company created by three web developers who like cats and clothes. We wanted to produce top-fashion and high-quality clothing to sell as a hobby in an online store that we developed. We enjoyed the experience of making this ecommerce website as it was so entertaining. We hope you find products of your liking with each click.</p>
+          <p className="Description-About"><span style={{ color: "#623BC1", fontSize: "20px", fontWeight: "bolder"}}>BAM CAT</span> is a company created by three web developers who like cats and clothes. We wanted to produce top-fashion and high-quality clothing to sell as a hobby in an online store that we developed. We enjoyed the experience of making this ecommerce website as it was so entertaining. We hope you find products you like in each click.</p>
           <img style={{ width: "150px" }} src={logo}></img>
         </div>
         <div className="Image-Container" >
