2e4aefd4067a3885e477a4c147d57340929eed8c
Nastasiia-Smr <volhvina@outlook.com>
2021-06-28 17:21:34 +0200

    skeleton got fixed

--- src/features/Components/CardSkeleton.js
+++ src/features/Components/CardSkeleton.js
@@ -1,18 +1,33 @@
 import Skeleton from "react-loading-skeleton";
+import { Card, Button } from "react-bootstrap";
+import "../styles/Card.css";
 
 function CardSkeleton() {
   return (
-    <div className="Card">
-      <Skeleton height={150} />
-      <h1>
-      <Skeleton height={20} />
-      </h1>
-      <p className="Price">
-        <Skeleton height={20} />
-      </p>
+    <Card style={{ width: "18rem", padding: "0.5rem" }}>
+      <Skeleton height={250} />
+
       <Skeleton height={30} />
-      <Skeleton height={30} />
-    </div>
+
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+
+      <Card.Body
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Button disabled style={{ cursor: "default" }}></Button>
+          <Button disabled style={{ cursor: "default" }}></Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
 
