cc405dbd20e619bf48635ea136a328b9d88feb46
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-06-28 14:01:11 +0200

    Adding some styling to Hero image section at Home Page

--- src/features/Pages/HomePage.js
+++ src/features/Pages/HomePage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "../styles/HomePage.css";
+import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchProducts } from "../../actions/ProductAction";
-import hero from "../img/hero.jpg"
-import hero1 from "../img/hero1.jpg"
-import hero2 from "../img/hero2.jpg"
-import hero3 from "../img/hero3.jpg"
-import Carousel from 'react-bootstrap/Carousel'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import hero from "../img/hero.jpg";
+import hero1 from "../img/hero1.jpg";
+import hero2 from "../img/hero2.jpg";
+import hero3 from "../img/hero3.jpg";
+import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function HomePage() {
   const dispatch = useDispatch();
@@ -19,70 +21,72 @@ function HomePage() {
 
   return (
     <div className="Carousel">
-  <Carousel>
-  <Carousel.Item interval={5000}>
-    <img
-      className="d-block w-100"
-      src={hero}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={5000}>
-    <img
-      className="d-block w-100"
-      src={hero1}
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={5000}>
-    <img
-      className="d-block w-100"
-      src={hero2}
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={5000}>
-    <img
-      className="d-block w-100"
-      src={hero3}
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-       {/* <HeroCarousel>
-    <img
-      src={hero}
-      alt=""
-    />
-    <img
-      src={hero1}
-      alt=""
-    />
-    <img
-      src={hero2}
-      alt=""
-    />
-    <img
-      src={hero3}
-      alt=""
-    />
-  </HeroCarousel> */}
+      <Carousel fade>
+        <Carousel.Item interval={3000}>
+          <img className="d-block w-100" src={hero} alt="First slide" />
+          <Carousel.Caption>
+            <h3>FASHION</h3>
+            <h3>CHANGING</h3>
+            <h3>ALWAYS</h3>
+            <button>
+            <NavLink
+              className="home-shop"
+              exact="true"
+              to="/shop"
+            >
+              SHOP NOW
+            </NavLink></button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img className="d-block w-100" src={hero1} alt="Second slide" />
+          <Carousel.Caption>
+            <h3>FASHION</h3>
+            <h3>CHANGING</h3>
+            <h3>ALWAYS</h3>
+            <Button>
+            <NavLink
+              className="home-shop"
+              exact="true"
+              to="/shop"
+            >
+              SHOP NOW
+            </NavLink></Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img className="d-block w-100" src={hero2} alt="Third slide" />
+          <Carousel.Caption>
+            <h3>FASHION</h3>
+            <h3>CHANGING</h3>
+            <h3>ALWAYS</h3>
+            <Button>
+            <NavLink
+              className="home-shop"
+              exact="true"
+              to="/shop"
+            >
+              SHOP NOW
+            </NavLink></Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img className="d-block w-100" src={hero3} alt="Third slide" />
+          <Carousel.Caption>
+            <h3>FASHION</h3>
+            <h3>CHANGING</h3>
+            <h3>ALWAYS</h3>
+            <Button>
+            <NavLink
+              className="home-shop"
+              exact="true"
+              to="/shop"
+            >
+              SHOP NOW
+            </NavLink></Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
