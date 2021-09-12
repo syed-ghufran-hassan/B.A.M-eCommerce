e843844155cf928a8a042bb9a81124b147c97e4d
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-09-12 14:43:40 +0200

    Deploy server in heroku

--- client/src/env.js
+++ client/src/env.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://cat-b-a-m.herokuapp.com/";
--- server/index.js
+++ server/index.js
@@ -27,12 +27,12 @@ app.use("/contact", contact);
 app.use(express.static('public'));  
 app.use('/images', express.static('images')); 
 
-const CONNECTION_URL = "mongodb+srv://bam_user:bamenter1234@cluster0.ejkoy.mongodb.net/bam-project?retryWrites=true&w=majority"
+// const CONNECTION_URL = "mongodb+srv://bam_user:bamenter1234@cluster0.ejkoy.mongodb.net/bam-project?retryWrites=true&w=majority"
 
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server running on Port: http://localhost:${PORT}`)))
   .catch(error => console.log(`${error} did not connect`));
